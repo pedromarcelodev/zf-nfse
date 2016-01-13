e9f10e4a6cfb50df455d6a4a1cafcc583312e9f3
pedromarcelojava <pedromarcelodesaalves@gmail.com>
2016-01-12 22:02:52 -0200

    Added: XML Entity

---
+++ src/NFSe/XML/Entity/AbstractXMLEntity.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace NFSe\XML\Entity;
+
+/**
+ * Description of AbstractXMLEntity
+ *
+ * @author Pedro Marcelo
+ */
+abstract class AbstractXMLEntity extends AbstractEntity implements XMLEntityInterface
+{
+}
---
+++ src/NFSe/XML/Entity/XMLEntityInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace NFSe\XML\Entity;
+
+/**
+ *
+ * @author Pedro Marcelo
+ */
+interface XMLEntityInterface
+{
+    public function toXML();
+}
