a2f7c88f40ce7e6ad7933971bb480f49616f4847
pedromarcelojava <pedromarcelodesaalves@gmail.com>
2016-01-20 21:41:54 -0200

    Added: Factories to build instances of formatters

--- config/module.config.php
+++ config/module.config.php
@@ -7,21 +7,30 @@ namespace NFSe;
  * @package NFSe
  */
 return array(
-    "controllers" => array(
-        "invokables" => array(),
-    ),
-
-    // The following section is new and should be added to your file
-    "router" => array(
-        "routes" => array(),
-    ),
-
-    "view_manager" => array(
-        "template_path_stack" => array(
-            "nfse" => __DIR__ . "/../view",
+    "service_manager" => array(
+        "factories" => array(
+            "NFSe\Formatter\Date" => function ($serviceManager) {
+                $datetimeFormatter = new Formatter\DateTimeFormatter();
+                $datetimeFormatter->setPattern("Y-m-d");
+                return $datetimeFormatter;
+            },
+            "NFSe\Formatter\DateTime" => function ($serviceManager) {
+                $datetimeFormatter = new Formatter\DateTimeFormatter();
+                $datetimeFormatter->setPattern("Y-m-d\TH:i:s");
+                return $datetimeFormatter;
+            },
+            "NFSe\Formatter\Decimal" => function ($serviceManager) {
+                $decimalFormatter = new Formatter\DecimalFormatter();
+                $decimalFormatter->setPattern('+9.99');
+                return $decimalFormatter;
+            },
+            "NFSe\Formatter\Percentual" => function ($serviceManager) {
+                $decimalFormatter = new Formatter\DecimalFormatter();
+                $decimalFormatter->setPattern('9.9999');
+                return $decimalFormatter;
+            },
         ),
     ),
-
     "nfse" => array(
         "xml" => array(
             "map" => array(
