b879e3c5e38368f60dd00388437b24ab812008be
pedromarcelojava <pedromarcelodesaalves@gmail.com>
2016-01-23 00:11:17 -0200

    First entity of the documentation: TsNumeroNfse

---
+++ src/NFSe/XML/Entity/SimpleType/NfseNumber.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace NFSe\XML\Entity\SimpleType;
+
+/**
+ *
+ * @author Pedro Marcelo
+ */
+class NfseNumber extends GenericEntity
+{
+    protected $tagName = "TsNumeroNfse";
+}
---
+++ src/NFSe/XML/Factory/SimpleType/NfseNumberFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace NFSe\XML\Factory\SimpleType;
+
+/**
+ *
+ * @author Pedro Marcelo
+ */
+class NfseNumberFactory extends GenericFactory
+{
+    protected $entityTagName = 'TsNumeroNfse';
+}
