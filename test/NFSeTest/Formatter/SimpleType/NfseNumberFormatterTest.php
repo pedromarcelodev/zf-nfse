0a33171e5fd3b5ff24ea7f06ef5df3934fe51f70
pedromarcelojava <pedromarcelodesaalves@gmail.com>
2016-01-23 15:33:47 -0200

    Updated: using service manager to get NfseNumberFormatter instance

--- test/NFSeTest/Formatter/SimpleType/NfseNumberFormatterTest.php
+++ test/NFSeTest/Formatter/SimpleType/NfseNumberFormatterTest.php
@@ -13,20 +13,21 @@ class NfseNumberFormatterTest extends \PHPUnit_Framework_TestCase
 {
     public function testFormatValue()
     {
-        $nfseNumberFormatter = new NfseNumberFormatter();
-        $this->assertEquals("201412345678901", $nfseNumberFormatter->format("201412345678901"));
-        $this->assertEquals("201512345678901", $nfseNumberFormatter->format("201512345678901"));
-        $this->assertEquals("201612345678901", $nfseNumberFormatter->format("201612345678901"));
+        /* @var $formatter \NFSe\Formatter\FormatterInterface */
+        $formatter = \NFSeTest\Bootstrap::getServiceManager()->get("NFSe\Formatter\NFSeNumber");
+        $this->assertEquals("201412345678901", $formatter->format("201412345678901"));
+        $this->assertEquals("201512345678901", $formatter->format("201512345678901"));
+        $this->assertEquals("201612345678901", $formatter->format("201612345678901"));
         
         try {
-            $nfseNumberFormatter->format("201712345678901");
+            $formatter->format("201712345678901");
             $this->fail("2017 was accepted");
         } catch (FormatterException $ex) {
             $this->assertEquals("The year is not a current year and not earlier. Informed year: 2017", $ex->getMessage());
         }
         
         try {
-            $nfseNumberFormatter->format("201712345601");
+            $formatter->format("201712345601");
             $this->fail("The invalid format was accepted");
         } catch (FormatterException $ex) {
             $this->assertEquals(
