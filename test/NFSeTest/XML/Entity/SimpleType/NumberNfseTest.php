df3e0e88f5f941e52fc3681b113a7f7049c9b325
pedromarcelojava <pedromarcelodesaalves@gmail.com>
2016-01-23 13:30:52 -0200

    Updated: Extending from the PHPUnit class

--- test/NFSeTest/XML/Entity/SimpleType/NumberNfseTest.php
+++ test/NFSeTest/XML/Entity/SimpleType/NumberNfseTest.php
@@ -6,13 +6,32 @@ namespace NFSeTest\XML\Entity\SimpleType;
  *
  * @author Pedro Marcelo
  */
-class NumberNfseTest
+class NumberNfseTest extends \PHPUnit_Framework_TestCase
 {
+    const TAG_NAME = 'TsNumeroNfse';
+    
+    const CLASS_NAME = '\NFSe\XML\Entity\SimpleType\NfseNumber';
+    
+    const VALID_VALUE = '201612345678901';
+    
+    const INVALID_VALUE = 'Value';
+    
+    const EXPECTED_EXCEPTION1 = '\NFSe\Formatter\FormatterException';
+    
+    public function testIsInstanceOf()
+    {
+        /* @var $entityManager \NFSe\Service\EntityManager */
+        $entityManager = \NFSeTest\Bootstrap::getServiceManager()->get('NFSe\Service\EntityManager');
+        $entity = $entityManager->get(self::TAG_NAME);
+        $this->assertTrue(class_exists(self::CLASS_NAME));
+        $this->assertInstanceOf(self::CLASS_NAME, $entity);
+    }
+    
     public function testSetAttributes()
     {
         /* @var $entityManager \NFSe\Service\EntityManager */
         $entityManager = \NFSeTest\Bootstrap::getServiceManager()->get('NFSe\Service\EntityManager');
-        $entity = $entityManager->get('TsNumeroNfse');
+        $entity = $entityManager->get(self::TAG_NAME);
         $entity->setAttribute('attr1', 'value1');
         $entity->setAttribute('attr2', 'value2');
         $entity->setAttribute('attr3', 'value3');
@@ -24,7 +43,7 @@ class NumberNfseTest
     {
         /* @var $entityManager \NFSe\Service\EntityManager */
         $entityManager = \NFSeTest\Bootstrap::getServiceManager()->get('NFSe\Service\EntityManager');
-        $entity = $entityManager->get('TsNumeroNfse');
+        $entity = $entityManager->get(self::TAG_NAME);
         $entity->setAttribute('attr1', 'value1');
         $entity->setAttribute('attr2', 'value2');
         $entity->setAttribute('attr2', 'value3');
@@ -36,7 +55,7 @@ class NumberNfseTest
     {
         /* @var $entityManager \NFSe\Service\EntityManager */
         $entityManager = \NFSeTest\Bootstrap::getServiceManager()->get('NFSe\Service\EntityManager');
-        $entity = $entityManager->get('TsNumeroNfse');
+        $entity = $entityManager->get(self::TAG_NAME);
         $entity->setAttribute('attr1', 'value1');
         $entity->setAttribute('attr2', 'value2');
         $entity->setAttribute('attr3', 'value3');
@@ -50,7 +69,7 @@ class NumberNfseTest
     {
         /* @var $entityManager \NFSe\Service\EntityManager */
         $entityManager = \NFSeTest\Bootstrap::getServiceManager()->get('NFSe\Service\EntityManager');
-        $entity = $entityManager->get('TsNumeroNfse');
+        $entity = $entityManager->get(self::TAG_NAME);
         $entity->setAttribute('attr1', 'value1');
         $entity->setAttribute('attr2', 'value2');
         $entity->setAttribute('attr3', 'value3');
@@ -63,8 +82,10 @@ class NumberNfseTest
     {
         /* @var $entityManager \NFSe\Service\EntityManager */
         $entityManager = \NFSeTest\Bootstrap::getServiceManager()->get('NFSe\Service\EntityManager');
-        $entity = $entityManager->get('TsNumeroNfse');
-        $entity->setValue('Value');
-        $this->assertEquals('Value', $entity->getValue());
+        $entity = $entityManager->get(self::TAG_NAME);
+        $entity->setValue(self::VALID_VALUE);
+        $this->assertEquals('201612345678901', $entity->getValue());
+        $this->setExpectedException(self::EXPECTED_EXCEPTION1);
+        $entity->setValue(self::INVALID_VALUE);
     }
 }
